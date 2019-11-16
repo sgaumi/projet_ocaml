ed899f335067b8cd4da2784311dde25ffa88605b
siméon <gaumart@etud.insa-toulouse.fr>
2019-11-16 18:30:04 +0100

    v1 build_network

--- fordfulkerson.ml
+++ fordfulkerson.ml
@@ -6,6 +6,13 @@ type label =  {flow : int ; cap : int}
 
 let convert_tolabel gr = gmap gr (function i -> lab = {flow = 0; cap = i}) 
 
-let build_network gr =
-    gmap gr (function r:label -> (r.cap - r.flow))
-
+let build_network gr = 
+	let graph = gmap gr (function r:label -> (r.cap - r.flow))
+	in
+	(*let recup_flow id1 id2 gra = match find_arc id1 id2 gra with
+									|Some x -> x.flow
+									|None -> 0 
+	in*)
+	let add_arc_inv grr id1 id2 (i:label) = new_arc grr id2 id1 i.flow
+	in 
+	e_fold gr add_arc_inv graph
